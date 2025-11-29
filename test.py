31ab132b26a5babd860a23ae78d2eb4ef0f00499
Isaac Ding <isaac.ding@gmail.com>
2025-11-28 20:37:57 -0800

    update

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import cv2
 from ultralytics import YOLO
 from wincam import DXCamera
 from input_manager import InputManager, filter_input
-from utils import update_buffer_svd, random_projection
+from util import update_buffer_svd, random_projection
 import torch
+import numpy as np
 
 
 model = YOLO('./yolo/best.pt')
+model2 = YOLO('./yolo/best2.pt')
 window_title = 'Street Fighter 6'
 # print(gw.getAllTitles())
 
@@ -34,7 +36,8 @@ with DXCamera(0, 0, 1920, 1080, fps=30) as camera:
                 x1, y1, x2, y2 = b.xyxy[0].int().tolist()
                 cls = int(b.cls[0])
                 conf = float(b.conf[0])
-
+                frame = np.ascontiguousarray(frame)
+                frame = frame.astype(np.uint8, copy=False)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
                 if cls < 10:
                     opponent_state = cls
@@ -48,9 +51,9 @@ with DXCamera(0, 0, 1920, 1080, fps=30) as camera:
                     
                     
         embed_layers = [10]
-        embed = model.predict(frame, embed=embed_layers)[0]  
+        embed = model2.predict(frame, embed=embed_layers)[0]  
         
-        buffer, cache32 = update_buffer_svd(buffer=buffer, new_embed=embed, window_size=6, out_dim=32)
+        # buffer, cache32 = update_buffer_svd(buffer=buffer, new_embed=embed, window_size=6, out_dim=32)
         
         embed = random_projection(embed=embed, out_dim=64, file="random_projection_256_64.npy")
         
@@ -66,7 +69,7 @@ with DXCamera(0, 0, 1920, 1080, fps=30) as camera:
         # predictions = filter_input(actor_state, opponent_state) * prediction_unfiltered
         # prediction = argmax(predictions)
         # somehow update rl_model with filter_input mask
-        input_manager.update_facing(actor_bbox, opponenet_bbox)
+        # input_manager.update_facing(actor_bbox, opponenet_bbox)
         # input_manager.accept_prediction(prediction)
         # input_manager.output_actions()
 
